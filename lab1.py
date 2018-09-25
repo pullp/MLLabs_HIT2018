ea828ff6959fc8ebe37828afb396b2f4056a7ba5
Pullp <weixiankui1997@gmail.com>
2018-09-25 20:32:21 +0800

    problems with grad_dec on sin(2*pi*x)

--- lab1.py
+++ lab1.py
@@ -4,21 +4,21 @@ import matplotlib.pyplot as plt
 import time
 
 start, end, step = -1, 1, 0.05
-noise_range = 0.05
+noise_range = 0.1
 
 
 # generate dots
 x = np.arange(start, end, step)
-y = np.sin(np.pi*x)
+y = np.sin(3*np.pi*x)
 y += np.random.normal(0, noise_range, len(y))
-plt.plot(x, y)
+plt.plot(x, y, "r+")
 
 target = np.mat(array(y).reshape(len(y), 1))
 
 
 def loss_reg(matrix, w, t, l):
     temp = matmul(matrix, w) -t
-    return 0.5*(matmul(temp.T, temp) + l*matmul(w.T, w))
+    return 0.5*(matmul(temp.T, temp) + l*matmul(w.T, w)).getA()[0][0]
 
 
 def loss(matrix, w, t):
@@ -46,31 +46,51 @@ def calc(m=10):
     calc_reg(m, l=0)
 
 
-
+def recalc_gama(matrix, w, grad):
+    ret = 0
+    min_loss = 1e100
+    for i in range(1, 10):
+        gama_cur = 10 ** (-i)
+        w_new = w - gama_cur*grad
+        lo = loss(matrix, w_new, target)
+        if(lo < min_loss):
+            min_loss = lo
+            ret = gama_cur
+    return ret
     
-def grad_dec_reg(m=30, gama = 1e-4, l = 1e-1):
+def grad_dec_reg(m=5, gama = 1e-3, l = 1e-5):
     matrix = np.mat(array([[i**j for j in range(0, m)] for i in x]))
-    w = np.mat([0 for i in range(0, m)]).T #start from 0
+    w = np.mat(np.random.normal(size=m)).T
     A = matmul(matrix.T, matrix) + l * np.eye(m)
     b = matmul(matrix.T, target)
 #    print(w.T)
-    lo = 10
-    while(lo >= 0.3):
-#        print(w.T)
-#        if(lo<0.5):
-#            gama = 1e-4*3
+    lo = loss(matrix, w, target)
+    lo_prev = lo + 10
+    loop = 0
+    grad = 2*matmul(A.T,matmul(A, w)-b)
+    while(abs(lo - lo_prev) > 1e-7):
+        if(abs(lo - lo_prev) < 1e-4):
+            gama = recalc_gama(matrix, w, grad)
+            print("larger gama:" + str(gama))
         grad = 2*matmul(A.T,matmul(A, w)-b)
-        w = w - gama*grad
-#        Ax = matmul(A, w)
-#        print("|Ax-b|^2 = ", matmul((Ax-b).T, (Ax-b)))
+        w -= gama*grad
+        lo_prev = lo
         lo = loss(matrix, w, target)
-#        print(lo)
+        while(lo >= lo_prev):
+            w += gama*grad
+            gama = 0.99*gama
+#            print("new gama:"+str(gama))
+            w -= gama*grad
+            lo = loss(matrix, w, target)
+#        gama = 1e-3
+        print("\rloss = " + str(lo) + " loop:" +str(loop), end='')
+        loop += 1
 #        print()
     out = matmul(matrix, w)
 #    print(w)
     plt.plot(x, out)
     
-def grad_dec(m=30, gama = 1e-4):
+def grad_dec(m=4, gama = 1e-3):
     grad_dec_reg(m, gama, l=0)
 
 
@@ -78,10 +98,10 @@ def grad_dec(m=30, gama = 1e-4):
 def con_grad(m=10):
     con_grad_reg(m, l=0)
     
-def con_grad_reg(m=10, lamda=1e-1):
+def con_grad_reg(m=10, l=1e-1):
     matrix = np.mat(array([[i**j for j in range(0, m)] for i in x]))
-    w = np.mat([0 for i in range(0, m)]).T #start from 0
-    A = matmul(matrix.T, matrix) + lamda * np.eye(m)
+    w = np.mat(np.random.normal(size=m)).T
+    A = matmul(matrix.T, matrix) + l * np.eye(m)
     b = matmul(matrix.T, target)
     w_array = [0 for i in range(0, 2*m)]
     r_array = [0 for i in range(0, 2*m)]
@@ -109,12 +129,13 @@ def con_grad_reg(m=10, lamda=1e-1):
 if __name__ == "__main__":
     time_start = time.time()
     m = 30
-    lamda= 1e-3
-#    calc_reg(m, lamda)
+    l= 1e-3
+#    calc_reg(20, 1e-5)
 #    calc(20)
-#    grad_dec(30)
-#    con_grad_reg()
-    grad_dec_reg()
+#    grad_dec(m=20, gama=1e-1)
+#    grad_dec_reg(gama = 1e-3)
+#    con_grad(m=20)
+    con_grad_reg(m=20, l=1e-5)
     time_end = time.time()
     print("use time:", time_end - time_start)
     
